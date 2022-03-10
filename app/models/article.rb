75c9a4ac573526c91aae622f3ecbce7fd6103081
Tim Jedrek <tim@timjedrek.com>
2022-03-10 00:17:15 -0600

    add status to article controller

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,12 @@ class Article < ApplicationRecord
 
   validates :title, presence: true, length: { minimum: 5 }
   validates :text, presence: true, length: { minimum: 10 }
+
+  VALID_STATUSES = ['public', 'private', 'archived']
+
+  validates :status, inclusion: { in: VALID_STATUSES }
+
+  def archived?
+    status == 'archived'
+  end
 end
