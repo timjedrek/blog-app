879ed87e452c8b2971bfe4e058fe0302ed173506
Tim Jedrek <tim@timjedrek.com>
2022-03-12 13:37:46 -0600

    add display count function

--- app/models/concerns/visible.rb
+++ app/models/concerns/visible.rb
@@ -6,6 +6,13 @@ module Visible
   included do
     validates :status, inclusion: { in: VALID_STATUSES }
   end
+
+  class_methods do
+    def public_count
+      where(status: 'public').count
+    end
+  end
+
   def archived?
     status == 'archived'
   end
