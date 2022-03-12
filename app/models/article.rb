4fd32817abf8aaaf00507d8396b0d713414280dd
Tim Jedrek <tim@timjedrek.com>
2022-03-12 13:35:45 -0600

    refactor visible check archive

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,14 +1,8 @@
 class Article < ApplicationRecord
+  include Visible
+
   has_many :comments
 
   validates :title, presence: true, length: { minimum: 5 }
   validates :text, presence: true, length: { minimum: 10 }
-
-  VALID_STATUSES = ['public', 'private', 'archived']
-
-  validates :status, inclusion: { in: VALID_STATUSES }
-
-  def archived?
-    status == 'archived'
-  end
 end
