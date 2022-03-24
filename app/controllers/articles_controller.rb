a2be01ac12742fa915f7853c114edd358ee390c6
Tim Jedrek <tim@timjedrek.com>
2022-03-23 21:44:24 -0500

    fix edit article by changing path in view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,11 +4,11 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
-  
+
   def show
     @article = Article.find(params[:id])
   end
-  
+
   def new
     @article = Article.new
   end
@@ -23,9 +23,9 @@ class ArticlesController < ApplicationController
     end
   end
 
-  #def edit
-  #  @article = Article.find(params[:id])
-  #end
+  def edit
+    @article = Article.find(params[:id])
+  end
 
   def update
     @article = Article.find(params[:id])
