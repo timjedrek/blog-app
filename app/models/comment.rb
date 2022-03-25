96ac5ffdba8f93c590000ace08d1b4a2e160410c
Tim Jedrek <tim@timjedrek.com>
2022-03-25 01:32:39 -0500

    add no blanks verification

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,7 @@ class Comment < ApplicationRecord
   #include Visible
 
   belongs_to :article
+
+  validates :commenter, presence: true, length: { minimum: 1 }
+  validates :body, presence: true, length: { minimum: 1 }
 end
